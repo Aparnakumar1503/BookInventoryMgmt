99c34d01e8d4b796b9d638a594f3a3c9dc5b9357
Aparnakumar1503 <154442409+Aparnakumar1503@users.noreply.github.com>
2026-04-14 22:06:50 +0530

    Book Inventory Mgmt Setup
    * Added Order Module 
    
    Co-authored-by: Janapriya <janapriya495@email.com>
    
    * inventory commit tested (#3)
    
    * feature reviewer added
    
    * Added book module
    
    ---------
    
    Co-authored-by: Janapriya07 <janapriya495@gmail.com>
    Co-authored-by: Janapriya <janapriya495@email.com>
    Co-authored-by: Sobika K <130832172+Sobika3@users.noreply.github.com>
    Co-authored-by: SWARNALATHA-D <swarnad2005@gmail.com>
    Co-authored-by: Lebazir Moses S <146012868+lebazirmoses@users.noreply.github.com>

--- src/main/java/com/sprint/BookInventoryMgmt/BookInventoryMgmtApplication.java
+++ src/main/java/com/sprint/BookInventoryMgmt/BookInventoryMgmtApplication.java
@@ -1,13 +1,21 @@
 package com.sprint.BookInventoryMgmt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BookInventoryMgmtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookInventoryMgmtApplication.class, args);
+
+		System.out.println("Order Module");
+		System.out.println("Inventory Module");
+		System.out.println("Reviewer Module Added");
+		System.out.println("hello");
+		System.out.println("Added Book Module");
 	}
 
 }
