708c70daa353067f2291f37c7150d77e98d88624
Aparnakumar1503 <aparnakumar027gmail.com>
2026-04-14 14:48:32 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/com/sprint/BookInventoryMgmt/BookInventoryMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.BookInventoryMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookInventoryMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookInventoryMgmtApplication.class, args);
+	}
+
+}
