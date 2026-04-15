0af27c3e6a6055e54c1d1da665cf814bc6fdb4ac
Sobika K <130832172+Sobika3@users.noreply.github.com>
2026-04-15 11:50:05 +0530

    Feature/inventory (#26)
    * inventory commit tested
    
    * Implemented Inventory module entities with validation and JPA repositories
    
    * added inventory entities and jpa repositories
    
    * added inventory entities and jpa repositories with validations

--- src/main/java/com/sprint/BookInventoryMgmt/BookInventoryMgmtApplication.java
+++ src/main/java/com/sprint/BookInventoryMgmt/BookInventoryMgmtApplication.java
@@ -1,15 +1,15 @@
 package com.sprint.BookInventoryMgmt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BookInventoryMgmtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookInventoryMgmtApplication.class, args);
-
-
 
 	}
 
