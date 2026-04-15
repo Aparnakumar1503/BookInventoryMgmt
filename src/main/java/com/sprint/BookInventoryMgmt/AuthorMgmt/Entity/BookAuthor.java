f1453a1e22be5e3f5b0b562f118b765c4ad6f23b
Aparnakumar1503 <154442409+Aparnakumar1503@users.noreply.github.com>
2026-04-15 13:08:02 +0530

    Feature/user (#28)
    * Intial SpringBoot Application Setup
    
    * Modules Created
    
    * Modules Added
    
    * Added AuthorMgmt Module
    
    ---------
    
    Co-authored-by: Aparnakumar1503 <aparnakumar027gmail.com>

--- src/main/java/com/sprint/BookInventoryMgmt/AuthorMgmt/Entity/BookAuthor.java
+++ src/main/java/com/sprint/BookInventoryMgmt/AuthorMgmt/Entity/BookAuthor.java
@@ -1,5 +1,26 @@
 package com.sprint.BookInventoryMgmt.AuthorMgmt.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "bookauthor")
+@IdClass(BookAuthorId.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookAuthor {
 
+	@Id
+	@Column(name = "ISBN")
+	private String isbn;
+
+	@Id
+	@Column(name = "AuthorID")
+	private Integer authorId;
+
+	@Column(name = "PrimaryAuthor")
+	private String primaryAuthor;
 }
+
