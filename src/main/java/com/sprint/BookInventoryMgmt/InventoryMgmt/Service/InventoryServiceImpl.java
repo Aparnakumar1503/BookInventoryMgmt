aef1996f8c3dcdfdd5105455e94353af60403b64
Sobika K <130832172+Sobika3@users.noreply.github.com>
2026-04-15 23:10:56 +0530

    Feature/inventory (#37)
    * inventory commit tested
    
    * Implemented Inventory module entities with validation and JPA repositories
    
    * added inventory entities and jpa repositories
    
    * added inventory entities and jpa repositories with validations
    
    * message
    
    * message
    
    * crud operations done
    
    * message
    
    ---------
    
    Co-authored-by: Aparnakumar1503 <154442409+Aparnakumar1503@users.noreply.github.com>

--- src/main/java/com/sprint/BookInventoryMgmt/InventoryMgmt/Service/InventoryServiceImpl.java
+++ src/main/java/com/sprint/BookInventoryMgmt/InventoryMgmt/Service/InventoryServiceImpl.java
@@ -1,52 +1,5 @@
 package com.sprint.BookInventoryMgmt.InventoryMgmt.Service;
 
-import com.sprint.BookInventoryMgmt.InventoryMgmt.Entity.Inventory;
-import com.sprint.BookInventoryMgmt.InventoryMgmt.Repository.InventoryRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
 public class InventoryServiceImpl implements InventoryService {
 
-    @Autowired
-    private InventoryRepository repository;
-
-    // GET ALL
-    @Override
-    public List<Inventory> getAllInventory() {
-        return repository.findAll();
-    }
-
-    // GET BY ID
-    @Override
-    public Inventory getInventoryById(Integer id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Inventory not found with id " + id));
-    }
-
-    // GET BY ISBN
-    @Override
-    public List<Inventory> getInventoryByISBN(String isbn) {
-        return repository.findByIsbn(isbn);
-    }
-
-    // ADD INVENTORY
-    @Override
-    public Inventory addInventory(Inventory inventory) {
-        inventory.setPurchased(false); // default value
-        return repository.save(inventory);
-    }
-
-    // MARK AS PURCHASED
-    @Override
-    public Inventory markAsPurchased(Integer id) {
-        Inventory inv = repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Inventory not found with id " + id));
-
-        inv.setPurchased(true);
-        return repository.save(inv);
-    }
 }
