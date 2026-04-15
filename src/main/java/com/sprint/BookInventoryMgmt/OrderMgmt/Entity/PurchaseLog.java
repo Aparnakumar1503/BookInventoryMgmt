e6add095582396971b4aa7cccc0c8bc00b4a27b7
Janapriya <janapriya495@email.com>
2026-04-15 11:20:30 +0530

    Tables Created

--- src/main/java/com/sprint/BookInventoryMgmt/OrderMgmt/Entity/PurchaseLog.java
+++ src/main/java/com/sprint/BookInventoryMgmt/OrderMgmt/Entity/PurchaseLog.java
@@ -4,10 +4,19 @@ package com.sprint.BookInventoryMgmt.OrderMgmt.Entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "purchase_log")
+
 public class PurchaseLog {
 
     @Id
