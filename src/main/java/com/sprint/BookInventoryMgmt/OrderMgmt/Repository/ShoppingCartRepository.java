0dd7a1beb71637ebfdd48db979ee3e0f7eca218e
Janapriya <janapriya495@email.com>
2026-04-15 11:35:02 +0530

    Repository Created

---
+++ src/main/java/com/sprint/BookInventoryMgmt/OrderMgmt/Repository/ShoppingCartRepository.java
@@ -0,0 +1,19 @@
+package com.sprint.BookInventoryMgmt.OrderMgmt.Repository;
+
+
+import com.sprint.BookInventoryMgmt.OrderMgmt.Entity.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+
+
+    List<ShoppingCart> findByUserId(Long userId);
+
+    List<ShoppingCart> findByIsbn(String isbn);
+
+    ShoppingCart findByUserIdAndIsbn(Long userId, String isbn);
+}
