30d6017d10166b814b700ad6f253a208238598fe
Aparnakumar1503 <154442409+Aparnakumar1503@users.noreply.github.com>
2026-04-16 11:03:14 +0530

    Test (#45)
    * Book module structure and initial setup
    
    * Added Book module entities and configured server port
    
    * feat(book): complete entity layer with validation and relationships
    
    * fix: resolved entity mapping and test issues
    
    * Fix: BookController tests and publisher FK issue resolved
    
    * Complete Working code after CRUD - JUNIT testing.
    
    * All modules are tested
    
    ---------
    
    Co-authored-by: lebazirmoses <lebazir2004@gmail.com>
    Co-authored-by: Aparnakumar1503 <aparnakumar027gmail.com>

--- src/main/java/com/sprint/BookInventoryMgmt/BookMgmt/Entity/State.java
+++ src/main/java/com/sprint/BookInventoryMgmt/BookMgmt/Entity/State.java
@@ -1,18 +1,7 @@
 package com.sprint.BookInventoryMgmt.BookMgmt.Entity;
 
-
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "state")
@@ -24,13 +13,9 @@ import lombok.Setter;
 public class State {
 
     @Id
-    @Column(name = "statecode", length = 2)
+    @Column(name = "state_code")
     private String stateCode;
 
-    @Column(name = "statename", length = 50)
+    @Column(name = "state_name")
     private String stateName;
-
-    // 🔁 One State → Many Publishers
-    @OneToMany(mappedBy = "state")
-    private List<Publisher> publishers;
 }
